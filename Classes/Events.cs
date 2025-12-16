be0f063e342a04189bb07832ff1e2ee6569a5bc9
ANosok <kirill60278@gmail.com>
2025-12-16 15:44:09 +0500

    Созданный классы для приложения.Черных

---
+++ Classes/Events.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TaskManagerTelegramBot_Chernykh.Classes
+{
+    public class Events
+    {
+        public DateTime Time { get; set; }
+        public string Message { get; set; }
+        public Events(DateTime time, string message)
+        {
+            Time = time;
+            Message = message;
+        }
+    }
+}
