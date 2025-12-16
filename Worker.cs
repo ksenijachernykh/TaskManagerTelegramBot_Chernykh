b7e0efa79c31f01e3f596c37213d00d48c07d796
ANosok <kirill60278@gmail.com>
2025-12-16 17:08:02 +0500

    Разработанные методы получения обновлений и ошибок.Черных

--- Worker.cs
+++ Worker.cs
@@ -1,5 +1,6 @@
 using TaskManagerTelegramBot_Chernykh.Classes;
 using Telegram.Bot;
+using Telegram.Bot.Polling;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
 using Telegram.Bot.Types.ReplyMarkups;
@@ -172,6 +173,34 @@ namespace TaskManagerTelegramBot_Chernykh
                     Time,
                     message.Text.Replace(Time.ToString("HH:mm dd.MM.yyyy") + "\n", "")));
             }
+        }
+
+        private async Task HandleUpdateAsync(
+            ITelegramBotClient client,
+            Update update,
+            CancellationToken cancellationToken)
+        {
+            if(update.Type == UpdateType.Message)
+            {
+                GetMessage(update.Message);
+            }
+            else if(update.Type == UpdateType.CallbackQuery)
+            {
+                CallbackQuery query = update.CallbackQuery;
+                Users User = Users.Find(x=> x.IdUser == query.Message.Chat.Id);
+                Events Event = User.Events.Find(x => x.Message == query.Data);
+                User.Events.Remove(Event);
+                SendMessage(query.Message.Chat.Id, 5);
+            }
+        }
+
+        private async Task HandleErrorAsync(
+            ITelegramBotClient client,
+            Exception exception,
+            HandleErrorSource source,
+            CancellationToken token)
+        {
+            Console.WriteLine("Ошибка: " + exception.Message);
         }
     }
 }
